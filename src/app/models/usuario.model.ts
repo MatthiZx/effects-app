a168f3c5c288d8ab9b89951d9e5cf1b59a42b17a
Matias Cofre <matias.cofrea@gmail.com>
2019-12-12 15:42:02 -0300

    primer commit

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,10 @@
+export class Usuario {
+   
+    constructor(
+           public id: number,
+           public first_name: string,
+           public plast_name: string,
+           public avatar: string
+    ){}
+
+}
